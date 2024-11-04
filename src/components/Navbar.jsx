f9486ef8b6878786580d2ed17d097e9ed4f459b0
MIREHMAN <Inammalik73@gmail.com>
2024-11-04 19:39:53 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
-import { LinkedIn } from "@mui/icons-material";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { LinkedIn, Mail, Message, Notifications } from "@mui/icons-material";
+import { AppBar, Avatar, Badge, Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import Search from "./Search";
 
@@ -12,7 +12,15 @@ const MyToolbar=styled(Toolbar)({
 
 const Icons=styled(Box)(({
     display:'flex',
-    backgroundColor:'white',
+    alignItems:'center',
+    border:'1px solid lightgray',
+    p:2,
+    borderRadius:'100px',
+    "&:hover":{
+        backgroundColor:'ButtonShadow',
+        cursor:'pointer',
+        borderRadius:'100px'
+    }
   
 
 }));
@@ -25,7 +33,25 @@ const Navbar = () => {
         </Box>
        
         <Search/>
-        <Icons>Icons</Icons>
+        <Box sx={{display:'flex', alignItems:'center', gap:2}}>
+        <Icons>
+            <Badge badgeContent={4} color="secondary">
+                <Notifications/>
+            </Badge>
+        </Icons>
+        <Icons>
+            <Badge badgeContent={3} color="secondary">
+                <Mail/>
+            </Badge>
+        </Icons>
+        <Icons>
+            <Avatar/>
+            
+        </Icons>
+        <Typography>John Doe</Typography>
+        </Box>
+      
+        
       </MyToolbar>
     </AppBar>
   );
