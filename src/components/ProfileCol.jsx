860f631dc170fc91475b8184e63f6db56c1ee64d
MIREHMAN <Inammalik73@gmail.com>
2024-11-05 16:28:35 +0500

    Update ProfileCol.jsx

--- src/components/ProfileCol.jsx
+++ src/components/ProfileCol.jsx
@@ -1,9 +1,63 @@
-import { Box } from '@mui/material'
+import { DarkMode, Groups, Home, MonochromePhotos, NightShelter, Pages, Settings, Shop, Shop2 } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 const ProfileCol = () => {
   return (
-    <Box flex={1} sx={{display:{xs:'none', md:'block'}}}>ProfileCol</Box>
+    <Box flex={1} sx={{display:{xs:'none', md:'block'}}}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton comonent='a' href='#'>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton comonent='a' href='#'>
+              <ListItemIcon>
+                <Pages />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton comonent='a' href='#'>
+              <ListItemIcon>
+                <Groups />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton comonent='a' href='#'>
+              <ListItemIcon>
+                <Shop2 />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton comonent='a' href='#'>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton comonent='a' href='#'>
+              <ListItemIcon>
+                <DarkMode/>
+              </ListItemIcon>
+              <Switch/>
+            </ListItemButton>
+          </ListItem>
+          
+          </List>
+          
+    </Box>
   )
 }
 
